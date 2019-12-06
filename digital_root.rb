1d7eae14690a86a87e0eb428819ec119dc94bc79
David Morton <55235981+davidmorton0@users.noreply.github.com>
2019-12-06 14:16:26 +0000

    add tests

--- digital_root.rb
+++ digital_root.rb
@@ -1,4 +1,10 @@
+require "test/unit/assertions"
+include Test::Unit::Assertions
+
 def digital_root(n)
   i = n.digits.sum
   i > 9 ? digital_root(i) : i
 end
+
+assert_equal( digital_root(16), 7 )
+assert_equal( digital_root(456), 6 )
